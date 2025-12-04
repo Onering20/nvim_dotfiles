39ea1270b2ae3314b510ef84bc494b64e80d171a
Onering20 <Onering20@gmail.com>
2025-12-03 22:46:00 -0600

    Fresh install

--- lua/plugins/general/wezterm.lua
+++ lua/plugins/general/wezterm.lua
@@ -6,6 +6,17 @@ return{
         enabled = true,
         lazy = false,
         opts = {},
+        --[[ config = function()
+            local wezterm = require('wezterm')
+            local mux = wezterm.mux
+
+            wezterm.on('gui-startup', function(cmd)
+                if cmd.args[1] == 'nvim' then
+                    local tab, pane, window = mux.spawn_window(cmd or {})
+                    window:gui_window():maximize()
+                end
+            end)
+        end, ]]
     },
 
 
